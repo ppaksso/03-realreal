87adb704489d84440fc7ea06f7bc9511500a948f
ppaksso <113421378+ppaksso@users.noreply.github.com>
2022-09-20 17:19:19 +0900

    3

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char text;
+	
+	printf("input a characer :");
+	scanf("%c",&text);
+	
+	printf("the next charater of %c (%d) is %c (%d)\n", text, text, text+1, text+1);
+	return 0;
+}
